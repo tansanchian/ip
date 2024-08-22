7646a41094e61d781685b40956c4a9d6e79b8e18
tansanchian <tansanchian040812@gmail.com>
2024-08-22 22:02:13 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Handsome.java
@@ -0,0 +1,7 @@
+public class Handsome {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Handsome\n" +
+                           "What can I do for you?\n\n" +
+                           "Bye. Hope to see you again soon!");
+    }
+}
