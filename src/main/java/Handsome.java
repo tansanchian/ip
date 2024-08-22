742fc169b93e98fcd0912002f4bd4f13a53426e0
tansanchian <tansanchian040812@gmail.com>
2024-08-22 22:31:12 +0800

    Echo

--- src/main/java/Handsome.java
+++ src/main/java/Handsome.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Handsome {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Handsome\n" +
-                           "What can I do for you?\n\n" +
-                           "Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Handsome\n" + "What can I do for you?\n");
+        String input = scanner.nextLine();
+        while (true) {
+            if (input.equals("greet")) {
+                System.out.println("Hello! I'm Handsome\n" + "What can I do for you?");
+                input = scanner.nextLine();
+            } else if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+                input = scanner.nextLine();
+            }
+        }
     }
 }
